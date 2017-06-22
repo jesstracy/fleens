f9c87dbd8d77c91f485a62085f87120ab08f6843
Jessica Tracy <j.tracy916@gmail.com>
2017-06-21 22:03:47 -0400

    generating fleen attributes from rand int differences

--- fleens.py
+++ fleens.py
@@ -1,18 +1,40 @@
-from creature import Zoombini
+from creature import Zoombini, Fleen
+import random
 
 NUM_ZOOMBINIS = 16
+HAIR_DIFF = random.randint(0, 4)
+EYE_DIFF = random.randint(0, 4)
+NOSE_DIFF = random.randint(0, 4)
+SHOES_DIFF = random.randint(0, 4)
 
 def main():
-	#Make 16 random zoombinis
+	#Make 16 random zoombinis and corresponding fleens
 	zoombinis = []
+	fleens = []
 	for i in range(0, NUM_ZOOMBINIS):
-		new_zoom = Zoombini()
+		h = random.randint(0, 4)
+		e = random.randint(0, 4)
+		n = random.randint(0, 4)
+		s = random.randint(0, 4)
+		new_zoom = Zoombini(i, h, e, n, s)
 		zoombinis.append(new_zoom)
+		#all fleen 0 hairs should correspond to zoombini 2
+		new_fleen = Fleen(i, (h + HAIR_DIFF) % 5, (e + EYE_DIFF) % 5, (n + NOSE_DIFF) % 5, (s + SHOES_DIFF) % 5)
+		fleens.append(new_fleen)
 
-	count = 1
-	for zoom in zoombinis:
-		print("%i- %i" % (count, zoom.hair))
-		count = count + 1
+	print("Hair difference: %i" % HAIR_DIFF)
+	for i in range(0, 3):
+		print("Zoombini%i hair: %i" % (i + 1, zoombinis[i].hair))
+		print("Fleen%i hair: %i" % (i + 1, fleens[i].hair))
+
+	print("")
+
+	print("Eye difference: %i" % EYE_DIFF)
+	for i in range(0, 3):
+		print("Zoombini%i eyes: %i" % (i + 1, zoombinis[i].eyes))
+		print("Fleen%i eyes: %i" % (i + 1, fleens[i].eyes))
+
+
 
 
 if __name__ == '__main__':
